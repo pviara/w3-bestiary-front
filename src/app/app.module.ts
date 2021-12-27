9daa0b2021dab57bfdc2f29b41ba1e0a958f6b0c
Pierre Viara <pierre.viara@aktome.fr>
2021-12-27 22:47:18 +0100

    feat(app): add just a little bit of structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LangSelectorComponent } from './components/lang-selector/lang-selector.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LangSelectorComponent
   ],
   imports: [
     BrowserModule,
