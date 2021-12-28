04d5f6ca6cfa3289ec6b2c773f3cccdfc47c4500
Pierre Viara <pierre.viara@aktome.fr>
2021-12-28 13:07:32 +0100

    feat(app): add local storage service and lang getters/setters

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LangSelectorComponent } from './components/lang-selector/lang-selector.component';
+import { LocalStorageService } from './services/local-storage.service';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,7 @@ import { LangSelectorComponent } from './components/lang-selector/lang-selector.
     BrowserModule,
     AppRoutingModule,
   ],
-  providers: [],
+  providers: [LocalStorageService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
