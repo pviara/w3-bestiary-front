739f2dcbf16233d39d2a438568e4b2a5b4118421
Pierre Viara <pierre.viara@aktome.fr>
2021-12-31 18:09:36 +0100

    feat(app): add multiple category selection

--- src/app/containers/components/monsters-menu/monsters-menu.component.ts
+++ src/app/containers/components/monsters-menu/monsters-menu.component.ts
@@ -9,7 +9,45 @@ export class MonstersMenuComponent {
   @Input()
   categories!: any[];
 
+  selectedCategories: string[] = [];
+  
+  selectedMonster!: string;
+
   assembleImagePath(monsterCode: string) {
     return `../../../../assets/bestiary/thumbnails/${monsterCode}.png`;
+  }
+
+  isCategorySelected(categoryName: string) {
+    return this
+      .selectedCategories
+      .some(
+        selectedCategory => selectedCategory === categoryName
+      );
+  }
+
+  toggleCategory(categoryName: string) {
+    const existingItem = this
+      .selectedCategories
+      .findIndex(
+        selectedCategory => selectedCategory === categoryName
+      );
+    if (this.selectedCategories[existingItem]) {
+      this
+        .selectedCategories
+        .splice(
+          existingItem,
+          1
+        );
+    } else {
+      this
+        .selectedCategories
+        .push(
+          categoryName
+        );
+    }
+  }
+  
+  selectMonster(monsterCode: string) {
+    this.selectedMonster = monsterCode;
   }
 }
