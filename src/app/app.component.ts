7fd8a39b24efc20d0b3a5e67cb3dd45c2161ef2a
Pierre Viara <pierre.viara@outlook.com>
2022-02-01 21:33:00 +0100

    feat(app): add countdown because it's fun

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'w3-bestiary-front';
+export class AppComponent implements OnDestroy, OnInit {
+  hasAppBeenReleased = false;
+
+  countdown!: string;
+  
+  private _timer: any;
+
+  ngOnDestroy() {
+    clearInterval(this._timer);
+  }
+  
+  ngOnInit() {
+    const releaseDate = new Date('2022-02-15T22:00:00');
+    
+    this.countdown = this._computeCountDown(releaseDate, new Date());
+    
+    this._timer = setInterval((_: any) => {
+      this.countdown = this._computeCountDown(releaseDate, new Date());
+    }, 1000);
+  }
+
+  private _computeCountDown(releaseDate: Date, now: Date) {
+    const difference = releaseDate.getTime() - now.getTime();
+    const parsedDate = new Date(difference);
+
+    return `
+      ${parsedDate.getDate()} days,
+      ${parsedDate.getHours()} hours,
+      ${parsedDate.getMinutes()} minutes,
+      ${parsedDate.getSeconds()} seconds
+    `;
+  }
 }
