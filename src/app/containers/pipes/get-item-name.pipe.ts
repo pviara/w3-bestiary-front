9c5e9b5943b1aec9d0776502292e6608093c7e0c
Pierre Viara <pierre.viara@outlook.com>
2022-03-25 17:52:56 +0100

    feat(weakspots): add directive with dynamic input data

--- src/app/containers/pipes/get-item-name.pipe.ts
+++ src/app/containers/pipes/get-item-name.pipe.ts
@@ -1,25 +1,17 @@
+import { Item } from 'src/app/models/item/item';
 import { Pipe, PipeTransform } from '@angular/core';
-import { map } from 'rxjs';
-import { ItemsService } from '../services/items.service';
 
 @Pipe({
     name: 'get_item_name'
 })
 export class GetItemNamePipe implements PipeTransform {
-    constructor(private _itemsService: ItemsService) {}
+    constructor() {}
     
-    transform(code: string) {
-        return this
-            ._itemsService
-            .getItems()
-            .pipe(
-                map(
-                    items => items
-                        .find(
-                            item => item.code === code
-                        )
-                        ?.name
-                )
-            );
+    transform(code: string, items: Item[]) {
+        return items
+            .find(
+                item => item.code === code
+            )
+            ?.name
     }
 }
