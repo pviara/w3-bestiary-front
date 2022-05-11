fe715a608f25732a84d5f70f245dc8217019da9b
Pierre Viara <pierre.viara@outlook.com>
2022-05-11 12:00:54 +0200

    fix(responsive): fix not working responsive monster menu

--- src/app/containers/monsters-explorer.component.ts
+++ src/app/containers/monsters-explorer.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { LocalStorageService } from '../services/local-storage.service';
 import { MonstersByCategory } from '../models/monster/monster';
 import { MonstersService } from './services/monsters.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'monsters-explorer',
@@ -14,11 +15,14 @@ export class MonstersExplorerComponent {
   hasMonsterBeenClicked = false;
   
   constructor(
+    private _route: ActivatedRoute,
     private _localStorageService: LocalStorageService,
     private _monstersService: MonstersService
   ) {}
 
   ngOnInit() {
+    this._hideMonsterMenu();
+    
     this._localStorageService
       .langSubject
       .subscribe(
@@ -34,5 +38,17 @@ export class MonstersExplorerComponent {
 
   setHasMonsterBeenClicked(event: boolean) {
     this.hasMonsterBeenClicked = event;
+    console.log(this.hasMonsterBeenClicked);
+  }
+
+  private _hideMonsterMenu() {
+    const monsterCode = this
+      ._route
+      .snapshot
+      .firstChild
+      ?.params['code'];
+    if (monsterCode) {
+      this.hasMonsterBeenClicked = true;
+    }
   }
 }
