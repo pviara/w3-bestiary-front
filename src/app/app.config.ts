84a8a2012f59c950081e46045bbe309721937104
Pierre Viara <pierre.viara@outlook.com>
2024-03-11 18:55:44 +0100

    tech: add SSR to project (#74)

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,17 +1,11 @@
-import { ApplicationConfig, isDevMode } from '@angular/core';
-import { provideClientHydration } from '@angular/platform-browser';
+import { ApplicationConfig } from '@angular/core';
 import { provideHttpClient, withFetch } from '@angular/common/http';
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
     providers: [
-        // // provideClientHydration(),
         provideHttpClient(withFetch()),
         provideRouter(routes),
-        // // provideServiceWorker('ngsw-worker.js', {
-        // //     enabled: !isDevMode(),
-        // //     registrationStrategy: 'registerWhenStable:30000',
-        // // }),
     ],
 };
