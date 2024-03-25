e0410a74ab7c2fc82d22f2c83cd1251e3b63e3eb
Pierre Viara <pierre.viara@outlook.com>
2024-03-25 10:32:50 +0100

    core: fix server execution

--- server.ts
+++ server.ts
@@ -1,31 +1,28 @@
-import 'zone.js/node';
-
 import { APP_BASE_HREF } from '@angular/common';
 import { CommonEngine } from '@angular/ssr';
 import express from 'express';
-import { existsSync } from 'node:fs';
-import { join } from 'node:path';
+import { fileURLToPath } from 'node:url';
+import { dirname, join, resolve } from 'node:path';
 import bootstrap from './src/main.server';
 
 // The Express app is exported so that it can be used by serverless Functions.
 export function app(): express.Express {
     const server = express();
-    const distFolder = join(process.cwd(), 'dist/w3-bestiary-front/browser');
-    const indexHtml = existsSync(join(distFolder, 'index.original.html'))
-        ? join(distFolder, 'index.original.html')
-        : join(distFolder, 'index.html');
+    const serverDistFolder = dirname(fileURLToPath(import.meta.url));
+    const browserDistFolder = resolve(serverDistFolder, '../browser');
+    const indexHtml = join(serverDistFolder, 'index.server.html');
 
     const commonEngine = new CommonEngine();
 
     server.set('view engine', 'html');
-    server.set('views', distFolder);
+    server.set('views', browserDistFolder);
 
     // Example Express Rest API endpoints
     // server.get('/api/**', (req, res) => { });
     // Serve static files from /browser
     server.get(
         '*.*',
-        express.static(distFolder, {
+        express.static(browserDistFolder, {
             maxAge: '1y',
         }),
     );
@@ -39,7 +36,7 @@ export function app(): express.Express {
                 bootstrap,
                 documentFilePath: indexHtml,
                 url: `${protocol}://${headers.host}${originalUrl}`,
-                publicPath: distFolder,
+                publicPath: browserDistFolder,
                 providers: [{ provide: APP_BASE_HREF, useValue: baseUrl }],
             })
             .then((html) => res.send(html))
@@ -50,7 +47,7 @@ export function app(): express.Express {
 }
 
 function run(): void {
-    const port = process.env['PORT'] || 4000;
+    const port = 4001;
 
     // Start up the Node server
     const server = app();
@@ -61,14 +58,4 @@ function run(): void {
     });
 }
 
-// Webpack will replace 'require' with '__webpack_require__'
-// '__non_webpack_require__' is a proxy to Node 'require'
-// The below code is to ensure that the server is run only when not requiring the bundle.
-declare const __non_webpack_require__: NodeRequire;
-const mainModule = __non_webpack_require__.main;
-const moduleFilename = (mainModule && mainModule.filename) || '';
-if (moduleFilename === __filename || moduleFilename.includes('iisnode')) {
-    run();
-}
-
-export default bootstrap;
+run();
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,7 +2,12 @@ import { ApplicationConfig } from '@angular/core';
 import { provideHttpClient, withFetch } from '@angular/common/http';
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
+import { provideClientHydration } from '@angular/platform-browser';
 
 export const appConfig: ApplicationConfig = {
-    providers: [provideHttpClient(withFetch()), provideRouter(routes)],
+    providers: [
+        provideClientHydration(),
+        provideHttpClient(withFetch()),
+        provideRouter(routes),
+    ],
 };
